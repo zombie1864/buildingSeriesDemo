96cd13dd8388d2efb6d09be6f71f76c21294fa75
zombie1864 <zombie1864@gmail.com>
2021-01-27 13:09:13 -0500

    added devDependencies to project

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
+"use strict";
 // const dummyData = require("../data/dummyData.json")
 // for (const entry in dummyData) {
 //     console.log(entry);
 // }
-var numbers = ["one", "two", "three"];
-for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
-    var num = numbers_1[_i];
+const numbers = ["one", "two", "three"];
+for (const num of numbers) {
     console.log(num);
 }
--- dist/server.js
+++ dist/server.js
@@ -1,15 +1,16 @@
-var http = require('http'); // npm i --save-dev @types/node, node has built-in lib called http, this is listening for the req 
-var nycEnergyData = require('../data/data.json'); // imports data.json
-var test = require('../src/index.js'); // this loads the js file to here 
+"use strict";
+const http = require('http'); // npm i --save-dev @types/node, node has built-in lib called http, this is listening for the req 
+const nycEnergyData = require('../data/data.json'); // imports data.json
+const test = require('../src/index.js'); // this loads the js file to here 
 // const fs = require('fs')
 // const js = "src/index.js"
 // const jsFile = fs.readFileSync(js)
-var server = http.createServer(function (req, res) {
+const server = http.createServer((req, res) => {
     if (req.url === '/api/test' && req.method === 'GET') { // sets the res being sent to a specific url and if the HTTP verb is 'GET'
         // res.setHeader('Content-type', 'application/json') // json is the formate that is being sent back 
         res.setHeader('Access-Control-Allow-Origin', '*'); // this is to help avoid CORS issues 
         res.writeHead(200, { 'Content-type': 'text/html' }); // HTTP status code 200, ok && html is the formate that is being sent back 
-        res.write("<html><script src=\"" + test + "\"></script><body>hello world</body></html>");
+        res.write(`<html><script src="${test}"></script><body>hello world</body></html>`);
         res.end();
     }
     else {
@@ -18,7 +19,7 @@ var server = http.createServer(function (req, res) {
         res.end(JSON.stringify({ errMsg: 'Invalid Url Route' }));
     }
 }); // end of server 
-var PORT = process.env.PORT || 8000; // the process.env prop is an obj which store and controls info about the env in which the process is currentlt running 
-server.listen(PORT, function () {
+const PORT = process.env.PORT || 8000; // the process.env prop is an obj which store and controls info about the env in which the process is currentlt running 
+server.listen(PORT, () => {
     console.log('listening on port 8000'); // this cb is an opt param 
 });
