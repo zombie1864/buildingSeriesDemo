132b510f631124aa0e56cc0ec1ffc70bf351ccc4
zombie1864 <zombie1864@gmail.com>
2021-02-01 16:49:08 -0500

    executed map idea, still in dev

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,32 @@
 import data from './data/data.json' // export of JSON by configuring ts with "resolveJsonModule": true
 
 class Main { // this is the blueprint for the main obj, used to structure the obj 
+    googleMapsApi: string 
+    // gmapsJsLib: string 
+    gmapsStyleTag: string 
+    gmapsDemo: string 
     style: string 
     tableTag: string
     dataObj: any
     mapContainer: string
     
     constructor() { // no params - reason: inst of main will simply produce a table 
+        this.googleMapsApi = '<script src="http://maps.google.com/maps/api/js?sensor=true"></script>'
+        // this.gmapsJsLib = '<script src="gmaps.core.js"></script>'
+        this.gmapsStyleTag = '<style type="text/css">    #map {\
+            width: 400px;\
+            height: 400px;\
+          }\
+        </style>'
+        this.gmapsDemo = '  <div id="map"></div><script>\
+         var map = new GMaps({ el : "#map", lat: -12.0433, lng: -77.0283, zoom: 12 });\
+         </script>'
         this.style = '<style>'
         this.tableTag = `<table style="border: 1px solid black; border-collapse: collapse">`
         this.dataObj = data
         this.mapContainer = '<div style="float:left;overflow:hidden">'
     }
-    
+
     tableComp = ():string => { // appends th to table tag 
         let result: string = this.tableTag; // gives the table tag 
         const arrOfColName = Object.keys(data.results[0]) // ds is [key1,...,key2]
